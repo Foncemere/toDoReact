c91572fa12b086c5af7597941e29dd8e81b187a6
Karl Taguibao <karltag2018@gmail.com>
2020-11-19 17:27:22 +0800

    Updates

---
+++ precache-manifest.e008197b50b2940e73b61f1b0615fef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4bbcd63df12e76774657ad9ef53205",
+    "url": "/toDoReact/index.html"
+  },
+  {
+    "revision": "ac464984de1e984dbbda",
+    "url": "/toDoReact/static/css/main.1d43c3fd.chunk.css"
+  },
+  {
+    "revision": "f21e0bb9161c6ea75a5b",
+    "url": "/toDoReact/static/js/2.ef4abc89.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/toDoReact/static/js/2.ef4abc89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac464984de1e984dbbda",
+    "url": "/toDoReact/static/js/main.a5609fdc.chunk.js"
+  },
+  {
+    "revision": "6af9bb54c6844729e92e",
+    "url": "/toDoReact/static/js/runtime-main.b12dd90d.js"
+  }
+]);
